f8ad32738023503600795294afc3a8a62c28de2b
Archan Banerjee <64304109+ArchanJS@users.noreply.github.com>
2021-07-21 14:09:22 +0530

    Update ladder.cpp

--- ladder.cpp
+++ ladder.cpp
@@ -62,3 +62,24 @@ int main(){
     cout<<s;
     return 0;
 }
+
+//4. Borze
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s,ans="";
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='.'){
+            ans+='0';
+        }
+        else{
+            if(s[i+1]=='.')ans+='1';
+            else ans+='2';
+            i++;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
